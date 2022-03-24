e7d17bd155161c5a5ba0b21bd56b317631a90011
Erik Mende <emende@student.hive.fi>
2022-03-24 16:11:31 +0200

    Added .gitignore, modified Makefile and main.c -file

--- Makefile
+++ Makefile
@@ -6,15 +6,31 @@
 #    By: emende <emende@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/22 12:46:17 by emende            #+#    #+#              #
-#    Updated: 2022/03/22 12:48:26 by emende           ###   ########.fr        #
+#    Updated: 2022/03/24 16:07:04 by emende           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = fdf
+CC = gcc
+CPPFLAGS = -I libft/includes -L libft -lft
+MLX_INCL = -I /usr/local/include -L /usr/local/lib -lmlx
+FRAMEWORKS = -framework OpenGL -framework AppKit
+OBJS = main.o
 
-all = $(NAME)
+all:  $(NAME)
 
-$(NAME):
-	
+$(NAME): $(OBJS)
+	make -C libft/
+	$(CC) -o $(NAME) $(OBJS) $(MLX_INCL) $(LIBFT_INCL) $(FRAMEWORKS)
 
-.PHONY all clean fclean re
+clean:
+	make clean -C libft/
+	rm -f $(OBJS)
+
+fclean: clean
+	make fclean -C libft/
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emende <emende@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 15:36:24 by emende            #+#    #+#             */
+/*   Updated: 2022/03/24 15:41:07 by emende           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include "libft.h"
+
+int	main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "FdF");
+	mlx_loop(mlx_ptr);
+	return (0);
+}
