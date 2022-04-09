d0f35500a8e4e81ceb1a08b0de53222bec62b2f4
Erik Mende <emende@student.hive.fi>
2022-04-09 18:48:23 +0300

    don't even try to free mlx_ptr

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 15:36:24 by emende            #+#    #+#             */
-/*   Updated: 2022/04/09 16:16:41 by emende           ###   ########.fr       */
+/*   Updated: 2022/04/09 18:47:18 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ void	panic(char	*msg, t_vars *v)
 			mlx_destroy_window(v->mlx_ptr, v->win_ptr);
 		if (v->arr && v->row_count > 0)
 			free_intarr(v->arr, v->row_count);
-		if (v->mlx_ptr)
-			free(v->mlx_ptr);
 		free(v);
 	}
 	exit (0);
